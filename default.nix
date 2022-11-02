a636973ee61fd7d1b142ade4667fae6b40a84ed8
Ruixi-rebirth <ruixirebirth@gmail.com>
2022-11-02 17:40:31 +0800

    Initial commit

---
+++ default.nix
@@ -0,0 +1,25 @@
+# This file describes your repository contents.
+# It should return a set of nix derivations
+# and optionally the special attributes `lib`, `modules` and `overlays`.
+# It should NOT import <nixpkgs>. Instead, you should take pkgs as an argument.
+# Having pkgs default to <nixpkgs> is fine though, and it lets you use short
+# commands such as:
+#     nix-build -A mypackage
+
+{ pkgs ? import <nixpkgs> { } }:
+
+{
+  # The `lib`, `modules`, and `overlay` names are special
+  lib = import ./lib { inherit pkgs; }; # functions
+  modules = import ./modules; # NixOS modules
+  overlays = import ./overlays; # nixpkgs overlays
+
+  bilibili_live_tui = pkgs.callPackage ./pkgs/bilibili_live_tui { };
+  catppuccin-cursors = pkgs.callPackage ./pkgs/catppuccin-cursors { };
+  catppuccin-latte-gtk = pkgs.callPackage ./pkgs/catppuccin-latte-gtk { };
+  catppuccin-frappe-gtk = pkgs.callPackage ./pkgs/catppuccin-frappe-gtk { };
+  fcitx5-pinyin-zhwiki = pkgs.callPackage ./pkgs/fcitx5-pinyin-zhwiki { };
+  fcitx5-pinyin-moegirl = pkgs.callPackage ./pkgs/fcitx5-pinyin-moegirl { };
+  # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
+  # ...
+}
